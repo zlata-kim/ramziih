b314953fb0d154af3c826770b175d6e7e33fa254
zlata-kim <148851208+zlata-kim@users.noreply.github.com>
2024-02-02 07:15:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function openLoginModal() {
+    document.getElementById('loginModal').style.display = 'block';
+}
+
+function closeLoginModal() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+function showSignUpForm() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('signupForm').style.display = 'block';
+}
